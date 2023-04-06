1dc70a3a587368630a5b704400a81cc51f767ec6
Shree-48 <shree.b@exalca.com>
2023-04-06 18:47:43 +0530

    Initial Commit

---
+++ LoginApi/Datas/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LoginApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginApi.Datas
+{
+    public class UserDbContext:DbContext
+    {
+        public UserDbContext(DbContextOptions options):base(options) 
+        {
+            
+        }
+        public DbSet<UserLoginClass> Logincred { get; set; }
+    }
+}

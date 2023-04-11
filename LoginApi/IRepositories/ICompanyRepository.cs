d99f71542c575bb20a9c7ea1a59236deb19277fb
Santhosh092001 <santhosh.m@exalca.com>
2023-04-11 16:03:37 +0530

    created Repository

---
+++ LoginApi/IRepositories/ICompanyRepository.cs
@@ -0,0 +1,13 @@
+﻿using LoginApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginApi.IRepositories
+{
+    public interface ICompanyRepository
+    {
+        public List<UserLoginClass> getuser();
+        public Task<UserLoginClass> authentication([FromBody] UserLoginClass checkuser);
+        public Task<bool> deleteuser(string username);
+        public Task createuser(UserLoginClass newuser);
+    }
+}
